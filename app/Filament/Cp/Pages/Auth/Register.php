31d438120b88c574f7829b3f56ff5ab293259d90
Mohammed Hamad <toshiba9895@gmail.com>
2024-09-08 15:22:46 +0400

    add Register.php

--- app/Filament/Cp/Pages/Auth/Register.php
+++ app/Filament/Cp/Pages/Auth/Register.php
@@ -15,6 +15,7 @@ use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Wizard;
+use Filament\Http\Responses\Auth\Contracts\RegistrationResponse;
 use Filament\Pages\Page;
 use Filament\Pages\Auth\Register as BaseRegister;
 use Illuminate\Support\Facades\Hash;
@@ -163,12 +164,14 @@ class Register extends BaseRegister
                             ->columnSpan([
                                 'sm' => 1,
                             ])
-                            ->submitAction(new HtmlString(html: '
-                        <button type="submit" class="filament-button filament-button-size-sm inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset dark:focus:ring-offset-0 min-h-[2rem] px-3 text-sm text-white shadow focus:ring-white border-transparent bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700">Register
-                        </button>'))
                     ])
                     ->statePath('data'),
             ),
         ];
+    }
+
+    public function register(): ?RegistrationResponse
+    {
+        dd($this->form->getState());
     }
 }
