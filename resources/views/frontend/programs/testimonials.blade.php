d3d944a8fbafb690b44283e7b45777fa641bb1a4
Mohammed Hamad <toshiba9895@gmail.com>
2024-10-31 06:32:02 +0400

    fix tesmoinal image

--- resources/views/frontend/programs/testimonials.blade.php
+++ resources/views/frontend/programs/testimonials.blade.php
@@ -15,7 +15,7 @@
                     <div dir="rtl" class="bg-white shadow-sm ring-1 ring-black/5 data-[dark]:bg-gray-800 data-[dark]:ring-white/15 p-5 rounded-lg w-[350px] max-w-full md:w-[450px] flex flex-col flex-shrink-0">
                         <div class="flex justify-between items-center">
                             <div class="flex items-center">
-                                <img src="/storage/{{  $evaluate->user->avatar_url }}" class="size-12 ml-3" alt="">
+                                <img src="{{  $evaluate->user->avatar_url ? '/storage/' . $evaluate->user->avatar_url : asset('images/default.jpg') }}" class="size-12 ml-3 rounded-full" alt="">
                                 <h4 class="text-md font-bold">{{ $evaluate->user->name }}</h4>
                             </div>
                             <img src="{{ asset('images/small-icons.svg') }}" class="size-16" alt="">
@@ -31,7 +31,7 @@
                 <div dir="rtl" class="bg-white shadow-sm ring-1 ring-black/5 data-[dark]:bg-gray-800 data-[dark]:ring-white/15 p-5 rounded-lg w-[350px] max-w-full md:w-[450px] flex flex-col flex-shrink-0">
                     <div class="flex justify-between items-center">
                         <div class="flex items-center">
-                            <img src="/storage/{{  $evaluate->user->avatar_url }}" class="size-12 ml-3" alt="">
+                            <img src="{{  $evaluate->user->avatar_url ? '/storage/' . $evaluate->user->avatar_url : asset('images/default.jpg') }}" class="size-12 ml-3 rounded-full" alt="">
                             <h4 class="text-md font-bold">{{ $evaluate->user->name }}</h4>
                         </div>
                         <img src="{{ asset('images/small-icons.svg') }}" class="size-16" alt="">
